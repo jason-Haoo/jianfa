e2cbfd12a75c5fe99cb14fde950b78727f445813
fenghao <19232@qq.com>
2020-09-18 21:20:34 +0800

    新的更新  02

--- src/main/java/com/atguigu/MyNewJianfa.java
+++ src/main/java/com/atguigu/MyNewJianfa.java
@@ -3,5 +3,7 @@ package com.atguigu;
 public class MyNewJianfa {
     public static void main(String[] args) {
         System.out.println("jianfawudi");
+        System.out.println("jianfawudi");
+        System.out.println("1231241244");
     }
 }
